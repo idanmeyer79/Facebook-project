3261ee5fcb7f2a6b14ab53e143ab52722a395f93
May Ashkenazi <mayas@mta.ac.il>
2022-11-14 21:07:36 +0200

    test

--- Date.h
+++ Date.h
@@ -3,7 +3,18 @@
 
 class Date
 {
+private: 
+	int day, month, year;
 
+public:
+	Date(int d, int m, int y);
+
+	void show() const;
 };
+
+Date::Date(int d, int m, int y) {
+	day = d;
+
+}
 
 #endif __DATE_H
