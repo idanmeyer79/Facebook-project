80ae110633a116ec1e699cc4ec16b906a6b45445
May Ashkenazi <mayas@mta.ac.il>
2022-11-14 21:10:45 +0200

    date

--- date.h
+++ date.h
@@ -1,5 +1,21 @@
-#pragma once
-class date
+#ifndef __DATE_H
+#define __DATE_H
+
+class Date
 {
+private: 
+	int day, month, year;
+
+public:
+	Date(int d, int m, int y);
+
+	void show() const;
 };
 
+Date::Date(int d, int m, int y) {
+	day = d;
+	month = m;
+	year = y;
+}
+
+#endif __DATE_H
