e59e215b6b9169607c59e3c44f66cf181e4ddf2d
Idan Meyer <idanmy@mta.ac.il>
2022-11-17 19:32:34 +0200

    added some method to some of the classes and added ctime, check out the example in the main

--- status.cpp
+++ status.cpp
@@ -1,1 +1,22 @@
 #include "status.h"
+
+status::status(char* text)
+{
+	time_t curr_time;
+	curr_time = time(NULL);
+	date_and_time = ctime(&curr_time);
+	this->text = text;
+}
+
+status::status()
+{
+	time_t curr_time;
+	curr_time = time(NULL);
+	date_and_time = ctime(&curr_time);
+	this->text = nullptr;
+}
+
+void status::printStatus()
+{
+	cout << date_and_time << endl << text;
+}
