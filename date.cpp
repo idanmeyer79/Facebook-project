b878a504ac8babc0419d7874a01d122efa4c89d4
Idan Meyer <idan@tulu.io>
2022-12-24 22:16:31 +0200

    date

--- date.cpp
+++ date.cpp
@@ -27,37 +27,6 @@ bool Date::setDate(int d, int m, int y)
     return true;
 }
 
-bool Date:: checkdate()
-{ 
-    if (!(1900 <= year))
-    {
-        return false;
-    }
-       
-    if (!(1 <= month && month <= 12))
-    {
-        return false;
-    }
-    if (!(1 <= day && day <= 31))
-    {
-        return false;
-    }
-    if ((day == 31) && (month == 2 || month == 4 || month == 6 || month == 9 || month == 11))
-    {
-        return false;
-    }  
-    if ((day == 30) && (month == 2))
-        {
-        return false;
-    }  
-    if ((month == 2) && (day == 29) && (year % 4 != 0))
-    {
-        return false;
-    }   
-
-    return true;
-}
-
 bool Date:: checkdate(int day, int month , int year)
 { 
     if (!(1900 <= year))
